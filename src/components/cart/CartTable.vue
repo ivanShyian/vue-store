53c1b17b10af71ca52a572885c3cb8f8657a286a
vanjkes <shyian.ivan@yahoo.com>
2021-02-23 19:47:24 +0200

    Connected global state of cart to all components requires cart

---
+++ src/components/cart/CartTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table">
+    <thead>
+    <tr>
+      <th>Наименование</th>
+      <th>Количество</th>
+      <th>Цена (шт)</th>
+    </tr>
+    </thead>
+    <tbody>
+      <cart-table-item v-for="item in bought"
+                       :key="item.id"
+                       :item="item"></cart-table-item>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import CartTableItem from '@/components/cart/CartTableItem'
+export default {
+  props: {
+    bought: {
+      type: Array,
+      required: false,
+      default: Array
+    }
+  },
+  setup() {
+    return {
+    }
+  },
+  components: { CartTableItem }
+}
+</script>
+
+<style scoped>
+
+</style>
