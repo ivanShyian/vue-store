a6a8e0ef21504b161412ba7be627afb8202b431d
vanjkes <shyian.ivan@yahoo.com>
2021-02-23 16:26:12 +0200

    Changed structure of cart

--- src/utils/cart.js
+++ src/utils/cart.js
@@ -1,9 +1,16 @@
 import store from '../store/index'
 
 export function addToCart(product, count = 1) {
-  const id = product.id
+  const uid = store.state.auth.uid
+  const prodId = product.id
+  const cart = store.getters['cart/cart'][uid] ? store.getters['cart/cart'][uid].list : null
   return {
-    ...store.getters['cart/cart'],
-    [id]: { product: product.title, count }
+    [uid]: {
+      date: Date.now(),
+      list: {
+        ...cart,
+        [prodId]: { product: product.title, count }
+      }
+    }
   }
 }
