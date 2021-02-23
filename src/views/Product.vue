624d11d14b97cbe25938737914dfba949d3a85ba
vanjkes <shyian.ivan@yahoo.com>
2021-02-23 03:08:18 +0200

    transfer product cart logic to cart

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,36 +1,45 @@
 <template>
-  <div class="breadcrumbs text-white" style="text-align: left">
-    <span @click="$router.push('/')"
-          style="cursor: pointer"
-    >Вернуться к товарам</span>
+  <app-loading v-if="loading"></app-loading>
+  <div v-else>
+    <div class="breadcrumbs text-white" style="text-align: left">
+      <span @click="$router.push('/')"
+            style="cursor: pointer"
+      >Вернуться к товарам</span>
+    </div>
+    <product-item v-if="product"
+                :product="product">
+    </product-item>
+    <h3 class="text-center text-white" v-else>
+      Товара не найден.
+    </h3>
   </div>
-  <app-product v-if="product"
-               :product="product">
-  </app-product>
-  <h3 class="text-center text-white" v-else>
-    Товара не найден.
-  </h3>
 </template>
 
 <script>
 import { useStore } from 'vuex'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
-import AppProduct from '@/components/product/ProductItem'
+import AppLoading from '../components/ui/AppLoading'
+import ProductItem from '../components/product/ProductItem.vue'
+
 export default {
   setup() {
     const store = useStore()
     const route = useRoute()
+    const loading = ref(false)
     onMounted(async () => {
+      loading.value = true
       await store.dispatch('products/loadProducts')
+      loading.value = false
     })
     return {
       product: computed(() => store.getters['products/products'].find(el => {
         return el.id === route.params.id
-      }))
+      })),
+      loading
     }
   },
-  components: { AppProduct }
+  components: { AppLoading, ProductItem }
 }
 </script>
 
